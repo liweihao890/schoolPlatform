3f0b3654f357a0c4b16fbd8f1af274a2125772a3
liweihao <13265317640@163.com>
2019-12-18 17:20:29 +0800

    对资源文章模块进行重构

--- controller/SourceController/SourceController.js
+++ controller/SourceController/SourceController.js
@@ -4,7 +4,260 @@ import { basename } from "path";
 import formidable from "formidable";
 class SourceController {
   constructor() {}
- 
+  /****获取文章的总文档数接口 */
+  async getSourceCount(req, res, next) {
+    try {
+      const count = await Source.countDocuments();
+      if (count) {
+        res.json({
+          status: 200,
+          result: count
+        });
+      } else {
+        return new Error(err);
+      }
+    } catch (e) {
+      return next(e);
+    }
+  }
+
+  /*****获取文章列表接口 */
+  async getSourceList(req, res, next) {
+    try {
+      //获取客户端发过来的当前页码和总页数
+      let page = Number.parseInt(req.query.page, 10) || 1;
+      let pageSize = Number.parseInt(req.query.pageSize, 10) || 3;
+      const sources = await Source.find()
+        .skip(pageSize * (page - 1))
+        .limit(pageSize)
+        .exec();
+      if (sources) {
+        res.json({
+          status: 200,
+          result: sources
+        });
+      } else {
+        return new Error(err);
+      }
+    } catch (e) {
+      return next(e);
+    }
+  }
+
+  /**** 添加图片接口* **/
+  async addSourceImg(req, res, next) {
+    const form = new formidable.IncomingForm();
+    form.uploadDir = config.uploadPath; // 上传图片放置的文件夹
+    form.keepExtensions = true; // 保持文件的原始扩展名
+    form.parse(req, async (err, fields, files) => {
+      try {
+        //检测到上传图片
+        if (files.image_url) {
+          //取出上传图片后再服务器的位置
+          let image_url = "uploads/" + basename(files.image_url.path);
+          //返回数据
+          res.json({
+            status: 200,
+            result: image_url
+          });
+        } else {
+          //上传失败
+          res.json({
+            status: 1,
+            result: "上传图片失败"
+          });
+        }
+      } catch (e) {
+        return next(err);
+      }
+    });
+  }
+
+  /****** 添加文章接口****/
+  async addSource(req, res, next) {
+    const form = new formidable.IncomingForm();
+    form.uploadDir = config.uploadPath; // 上传图片放置的文件夹
+    form.keepExtensions = true; // 保持文件的原始扩展名
+    form.parse(req, async (err, fields, files) => {
+      try {
+        // 1. 获取普通数据
+        let body = fields;
+        //2. 解析图片的路径,取出缩略图片图片的名字
+        body.small_img = basename(files.small_img.path);
+        //3. 生成数据文档
+        const source = new Source({
+          title: body.title,
+          author: body.author,
+          small_img: body.small_img,
+          price: body.price,
+          content: body.content
+        });
+        const result = await source.save();
+        if (result) {
+          res.json({
+            status: 200,
+            result: {
+              message: "添加文章成功!"
+            }
+          });
+        } else {
+          throw new Error("添加文章失败");
+        }
+      } catch (err) {
+        return next(err);
+      }
+    });
+  }
+
+  /***** 根据id修改文章接口*/
+  async updateSourceById(req, res, next) {
+    const form = new formidable.IncomingForm();
+    form.uploadDir = config.uploadPath; // 上传图片放置的文件夹
+    form.keepExtensions = true; // 保持文件的原始扩展名
+    form.parse(req, async (err, fields, files) => {
+      try {
+        // 1. 获取普通数据
+        let body = fields;
+
+        //2. 根据id查询数据库
+        const source = await Source.findById(body.id);
+        if (source) {
+          //修改数据
+          source.title = body.title;
+          source.author = body.author,
+          source.small_img = body.small_img || basename(files.small_img.path);
+          source.price = body.price;
+          source.content = body.content;
+          //3. 保存到数据库
+          const result =await source.save();
+          if (result) {
+            //返回结果给客户端
+            res.json({
+              status: 200,
+              result: {
+                message: "修改文章成功!"
+              }
+            });
+          }
+        } else {
+          throw new Error("修改文章失败");
+        }
+      } catch (error) {
+        return next(error);
+      }
+    });
+  }
+
+  /***** 根据id查询文章接口*/
+  async findSourceById (req, res, next) {
+      try {
+          const source = await Source.findById(req.params.sourceId);
+          if(source){
+              // 数据返回
+            res.json({
+                status: 200,
+                result: docs
+            });
+          }else{
+              throw new Error('查询文章失败!')
+          }
+      } catch (error) {
+          return next(error)
+      }
+    
+  }
+
+  /*** 根据id删除一篇文章接口 */
+  async deleteSourceById(req, res, next){
+      try {
+          const result = await Source.deleteOne({ _id: req.params.sourceId });
+          if(result){
+             // 数据返回
+             res.json({
+                status: 200,
+                result: "成功删除文章!"
+              });
+          }else{
+              throw new Error('删除文章失败')
+          }
+      } catch (error) {
+          return next(error)
+      }
+    
+  }
+
+  /*****前端获取文章总数接口 */
+  async getSourceCountByClient(req,res,next){
+      try {
+          const count = await Source.countDocuments();
+          if(count){
+            res.json({
+                status: 200,
+                result: count
+            })
+          }else{
+              throw new Error('暂时没有文章哟!')
+          }
+      } catch (error) {
+          return next(error)
+      }
+    
+ }
+
+ /****前端获取文章列表接口 */
+ async getSourceListByClient(req,res,next) {
+    //获取前端传过来的page,pageSize,排序规则
+    let {page,pageSize,sortBy} = req.query;
+    page = Number.parseInt(page, 10) || 1;
+     pageSize = Number.parseInt(pageSize, 10) || 100;
+     // 2. 数据查询规则
+     let sortObj;
+    //按照价格排序,高到低排序
+    if(sortBy === 'price'){
+         sortObj = {'price': -1}
+    }else{
+        //按照阅读量排序,高到低排序
+         sortObj = {'read_count': -1}
+    }
+    try {
+        const sources =await Source.find({}, 'title small_img price').sort(sortObj).skip((page - 1) * pageSize ).limit(pageSize).exec();
+        if(sources){
+            res.json({
+                status: 200,
+                result: sources
+            });
+        }else{
+            throw new Error('获取文章失败')
+        }
+    } catch (error) {
+        return next(error)
+    }
+   
+ }
+
+ /****前端获取文章详情接口 */
+ async getSourceDetailByClient(req,res,next) {
+     try {
+         const source = await Source.findById(req.params.sourceId)
+         if (source) {
+              // 1. 取出要修改的数据,阅读次数+1
+            source.read_count += 1;
+            const result =await source.save();
+            if(result){
+                   //根据id查询数据库
+             const detail = Source.findById(req.params.sourceId,"title author read_count add_time content");
+             if (detail) {
+                res.json({
+                    status: 200,
+                    result: source
+                })
+             }
+            }
+         }
+     } catch (error) {
+         return next(error)
+     }
+ }
 }
 
 export default new SourceController();
--- routes/source.js
+++ routes/source.js
@@ -1,292 +1,53 @@
 import express from "express";
-import Source from "./../models/Source";
-import config from "./../src/config";
-import { basename } from "path";
-import formidable from "formidable";
+import SourceController from './../controller/SourceController/SourceController'
 const router = express.Router({});
 
 /**************************接口API********************************/
 /****获取文章的总文档数接口 */
-router.get('/back/source/api/count',(req,res,next)=>{
-    Source.countDocuments((err,count) => {
-       if (err) {
-           return next(err)
-       }
-       res.json({
-           status: 200,
-           result: count
-       })
-    })
-})
+router.get('/back/source/api/count',SourceController.getSourceCount)
 
 /*****获取文章列表接口 */
-router.get('/back/source/api/list',(req,res,next) => {
-    //获取客户端发过来的当前页码和总页数
-  let page = Number.parseInt(req.query.page, 10) || 1;
-  let pageSize = Number.parseInt(req.query.pageSize, 10) || 3;
-  Source.find().skip(pageSize * (page - 1))
-  .limit(pageSize).exec((err,sources) => {
-     if (err) {
-         return next(err)
-     }
-     res.json({
-         status: 200,
-         result: sources
-     })
-  })
-})
+router.get('/back/source/api/list',SourceController.getSourceList)
 
-/**** 添加图片接口
- * **/
-router.post("/back/source/api/add_img", (req, res, next) => {
-  const form = new formidable.IncomingForm();
-  form.uploadDir = config.uploadPath; // 上传图片放置的文件夹
-  form.keepExtensions = true; // 保持文件的原始扩展名
-  form.parse(req, (err, fields, files) => {
-    if (err) {
-      return next(err);
-    }
-    //检测到上传图片
-    if (files.image_url) {
-      //取出上传图片后再服务器的位置
-      let image_url =
-        "http://localhost:3000" + "/uploads/" + basename(files.image_url.path);
-      //返回数据
-      res.json({
-        status: 200,
-        result: image_url
-      });
-    } else {
-      //上传失败
-      res.json({
-        status: 1,
-        result: "上传图片失败"
-      });
-    }
-  });
-});
+/**** 添加图片接口* **/
+router.post("/back/source/api/add_img", SourceController.addSourceImg);
 
-/****** 添加文章接口
- */
-router.post("/back/source/api/add", (req, res, next) => {
-  const form = new formidable.IncomingForm();
-  form.uploadDir = config.uploadPath; // 上传图片放置的文件夹
-  form.keepExtensions = true; // 保持文件的原始扩展名
-  form.parse(req, (err, fields, files) => {
-    if (err) {
-      return next(err);
-    }
-    // 1. 获取普通数据
-    let body = fields;
-    //2. 解析图片的路径,取出缩略图片图片的名字
-    body.small_img = basename(files.small_img.path);
-    //3. 生成数据文档
-    const source = new Source({
-      title: body.title,
-      author: body.author,
-      small_img: body.small_img,
-      price: body.price,
-      content: body.content
-    });
-    //4. 添加到数据库
-    source.save((err, result) => {
-      if (err) {
-        return next(err);
-      }
-      res.json({
-        status: 200,
-        result: {
-          message: "添加文章成功!"
-        }
-      });
-    });
-  });
-});
+/****** 添加文章接口*/
+router.post("/back/source/api/add",SourceController.addSource);
 
-/***** 根据id查询文章接口
- */
-router.get("/back/source/api/singer/:sourceId", (req, res, next) => {
-  Source.findById(req.params.sourceId, (err, docs) => {
-    if (err) {
-      return next(err);
-    }
-    // 数据返回
-    res.json({
-      status: 200,
-      result: docs
-    });
-  });
-});
+/***** 根据id查询文章接口*/
+router.get("/back/source/api/singer/:sourceId",SourceController.findSourceById);
 
-/***** 根据id修改文章接口
- */
-router.post("/back/source/api/edit", (req, res, next) => {
-  const form = new formidable.IncomingForm();
-  form.uploadDir = config.uploadPath; // 上传图片放置的文件夹
-  form.keepExtensions = true; // 保持文件的原始扩展名
-  form.parse(req, (err, fields, files) => {
-    if (err) {
-      return next(err);
-    }
-    // 1. 获取普通数据
-    let body = fields;
-    //2. 解析图片的路径,取出缩略图片图片的名字
-    body.small_img = basename(files.small_img.path);
-    //3. 根据id查询数据库
-    Source.findById(body.id, (err, source) => {
-      if (err) {
-        return next(err);
-      }
-      //修改数据
-      source.title = body.title;
-      (source.author = body.author),
-        (source.small_img = body.small_img || basename(files.small_img.path));
-      source.price = body.price;
-      source.content = body.content;
-      //4. 保存到数据库
-      source.save((err, result) => {
-        if (err) {
-          return next(err);
-        }
-        //返回结果给客户端
-        res.json({
-          status: 200,
-          result: {
-            message: "修改文章成功!"
-          }
-        });
-      });
-    });
-  });
-});
+/***** 根据id修改文章接口*/
+router.post("/back/source/api/edit", SourceController.updateSourceById);
 
-/*** 根据id删除一篇文章接口
- */
-router.get("/back/source/api/remove/:sourceId", (req, res, next) => {
-  Source.deleteOne({ _id: req.params.sourceId }, (err, result) => {
-    if (err) {
-      return next(err);
-    }
-    // 数据返回
-    res.json({
-      status: 200,
-      result: "成功删除文章!"
-    });
-  });
-});
+/*** 根据id删除一篇文章接口 */
+router.get("/back/source/api/remove/:sourceId", SourceController.deleteSourceById);
 
 /*****前端获取文章总数接口 */
-router.get('/web/source/api/count',(req,res,next) => {
-   Source.countDocuments((err,count) => {
-      if(err){
-          return next(err)
-      }
-      res.json({
-          status: 200,
-          result: count
-      })
-   })
-})
+router.get('/web/source/api/count',SourceController.getSourceCountByClient)
 
 /****前端获取文章列表接口 */
-router.get('/web/source/api/list',(req,res,next) => {
-   //获取前端传过来的page,pageSize,排序规则
-   let {page,pageSize,sortBy} = req.query;
-   page = Number.parseInt(page, 10) || 1;
-    pageSize = Number.parseInt(pageSize, 10) || 100;
-
-    // 2. 数据查询规则
-    let sortObj;
-   //按照价格排序,高到低排序
-   if(sortBy === 'price'){
-        sortObj = {'price': -1}
-   }else{
-       //按照阅读量排序,高到低排序
-        sortObj = {'read_count': -1}
-   }
-   // 数据库查询所有的数据
-   Source.find({}, 'title small_img price').sort(sortObj).skip((page - 1) * pageSize ).limit(pageSize).exec((err, sources)=>{
-    if(err){
-        return next(err);
-    }
-    res.json({
-        status: 200,
-        result: sources
-    });
-});
-})
+router.get('/web/source/api/list',SourceController.getSourceListByClient)
 
 /****前端获取文章详情接口 */
-router.get('/web/source/api/content/:sourceId',(req,res,next) => {
-   Source.findById(req.params.sourceId, "read_count", (err, source)=>{
-        if(err){
-            return next(err);
-        }
-        // 1. 取出要修改的数据,阅读次数+1
-        source.read_count += 1;
-        // 2. 保存
-        source.save((err, result)=>{
-            if(err){
-                return next(err);
-            }
-             //根据id查询数据库
-            Source.findById(req.params.sourceId,"title author read_count add_time content",(err,source) => {
-                if(err){
-                    return next(err)
-                }
-                res.json({
-                    status: 200,
-                    result: source
-                })
-            })
-        });
-    });
-  
-})
+router.get('/web/source/api/content/:sourceId',SourceController.getSourceDetailByClient)
 
 
 
 
 /**************************页面路由********************************/
 /** * 加载文章资源列表*/
-/* router.get("/back/source_list", (req, res, next) => {
-  //获取客户端发过来的当前页码和总页数
-  let page = Number.parseInt(req.query.page, 10) || 1;
-  let pageSize = Number.parseInt(req.query.pageSize, 10) || 3;
-  //利用分页查询的公式     startIndex = pageSize*(page-1)进行查询
-  Source.find()
-    .skip(pageSize * (page - 1))
-    .limit(pageSize)
-    .exec((err, sources) => {
-      if (err) {
-        return next(err);
-      }
-      //获取数据库文档的总数目
-      Source.countDocuments((err, count) => {
-        if (err) {
-          return next(err);
-        }
-         //获取总页数,向上取整
-         let totalPage = count ? Math.ceil(count / pageSize) : 1;
-        //给客户端返回总页数和当前页数,文章资源
-        res.render("back/source_list.html", { sources, totalPage, page });
-      });
-    });
-}); */
 router.get('/back/source_list',(req,res,next)=>{
     res.render('back/source_list.html')
 })
 
-/****
- * 加载添加文章页面
- */
+/***** 加载添加文章页面*/
 router.get("/back/source_add", (req, res, next) => {
   res.render("back/source_add.html");
 });
 
-/****
- * 加载编辑文章页面
- */
+/**** * 加载编辑文章页面*/
 router.get("/back/source_edit", (req, res, next) => {
   res.render("back/source_edit.html");
 });
